bd3588f89c94102ccb447ebc8d1b917ffcefa370
padme95 <emily-ss@hotmail.com>
2022-06-09 15:57:06 -0300

    Melhorias no update

--- src/main/java/br/com/works/crm/services/ClienteService.java
+++ src/main/java/br/com/works/crm/services/ClienteService.java
@@ -1,6 +1,7 @@
 package br.com.works.crm.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,10 @@ public class ClienteService {
 		return clienteRepository.findAll();
 	}
 	
+	public Cliente getOne(int id) {
+		return clienteRepository.findById(id).orElse(new Cliente());
+	}
+	
 	public Cliente save(Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
@@ -29,11 +34,17 @@ public class ClienteService {
 		
 	}
 	
-	public Cliente update(int id, Cliente cliente) {
- 		Cliente clienteAtual = clienteRepository.findById(id).get();
-		clienteAtual.setNome(cliente.getNome());
-//		BeanUtils.copyProperties(cliente, clienteAtual, "id"); 
-		return clienteRepository.save(clienteAtual);
+	public Cliente update(int id, Cliente novoCliente) {
+
+		Optional<Cliente> optional = clienteRepository.findById(id);
+		if(optional.isPresent()) {
+			Cliente cliente = optional.get();
+			cliente.setNome(novoCliente.getNome());
+			return clienteRepository.save(cliente);
+			}
+			return new Cliente();
+//	Outra Opção:	BeanUtils.copyProperties(cliente, clienteAtual, "id"); 
+// Esse código acima, faz a cópia dos valores de um para outro. de cliente para cliente atual, exceto o "id".
 		 
 	}
 }
